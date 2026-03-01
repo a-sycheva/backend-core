a54c9be402994131ecf9d1257911f222b2522d5e
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-03-01 11:29:11 +0300

    добавлен тест с Mockito для LeadService

--- src/test/java/ru/mentee/power/crm/service/LeadServiceTest.java
+++ src/test/java/ru/mentee/power/crm/service/LeadServiceTest.java
@@ -1,7 +1,7 @@
 package ru.mentee.power.crm.service;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.Optional;
