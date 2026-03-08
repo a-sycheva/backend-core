400a9acbaeba69d5c0876cce32afb0faa154796d
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-03-08 14:58:55 +0300

    добавила интеграционный тест с @SpringBootTest и MockMvc

--- src/test/java/ru/mentee/power/crm/spring/controller/LeadControllerTest.java
+++ src/test/java/ru/mentee/power/crm/spring/controller/LeadControllerTest.java
@@ -1,28 +1,28 @@
 package ru.mentee.power.crm.spring.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 class LeadControllerTest {
 
   @Autowired
-  TestRestTemplate testRestTemplate;
+  private MockMvc mockMvc;
 
   @Test
-  void shouldReturnHtmlTableWhenDoGetCalled() {
-    ResponseEntity<String> response = testRestTemplate.getForEntity("/leads", String.class);
+  void shouldReturnHtmlTableWhenDoGetCalled() throws Exception {
 
-    assertThat(response.getBody())
-        .contains("<title>CRM - Lead Management</title>")
-        .contains("Email</th>")
-        .contains("<p>&copy; 2025 CRM Project</p>");
-    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    mockMvc.perform(get("/leads"))
+            .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Email")));
   }
 }
