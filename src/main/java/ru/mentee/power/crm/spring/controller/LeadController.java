aa8660432eae87ee8ffdeb5cbb6c2e7e6f0ac01d
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-03-13 18:40:42 +0300

    добавлена возможность отбора лидов по статусу

--- src/main/java/ru/mentee/power/crm/spring/controller/LeadController.java
+++ src/main/java/ru/mentee/power/crm/spring/controller/LeadController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.mentee.power.crm.model.Lead;
+import ru.mentee.power.crm.model.LeadStatus;
 import ru.mentee.power.crm.service.LeadService;
 
 @Controller
@@ -17,9 +19,14 @@ public class LeadController {
   private final LeadService leadService;
 
   @GetMapping
-  public String showLeads(Model model) {
-    List<Lead> leads = leadService.findAll();
+  public String showLeads(
+      @RequestParam(required = false) LeadStatus status, Model model) {
+    List<Lead> leads = (status == null)
+        ? leadService.findAll()
+        : leadService.findByStatus(status);
+
     model.addAttribute("leads", leads);
+    model.addAttribute("currentFilter", status);
     return "leads/list";
   }
 }
