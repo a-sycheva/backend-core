e94b70d3f8063a8a59cef1a8276d2ed66ba182c7
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-03-17 13:32:28 +0300

    Unit-тест контроллера скорректирован под метод home

--- src/test/java/ru/mentee/power/crm/spring/controller/LeadControllerUnitTest.java
+++ src/test/java/ru/mentee/power/crm/spring/controller/LeadControllerUnitTest.java
@@ -2,31 +2,28 @@ package ru.mentee.power.crm.spring.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.ui.Model;
-import org.springframework.validation.support.BindingAwareModelMap;
-import ru.mentee.power.crm.model.Lead;
 import ru.mentee.power.crm.spring.MockLeadService;
 
 public class LeadControllerUnitTest {
-  MockLeadService mockService = new MockLeadService();
-  LeadController controller = new LeadController(mockService);
-  Model model = new BindingAwareModelMap();
-
   @Test
   void shouldCreateControllerWithoutSpring() {
-    String response = controller.showLeads(null, model);
-    List<Lead> leads = (List<Lead>) model.getAttribute("leads");
+    MockLeadService mockService = new MockLeadService();
 
-    assertThat(response).isEqualTo("leads/list");
-    assertThat(model.getAttribute("leads")).isInstanceOf(List.class);
-    assertThat(leads).hasSize(2);
+    LeadController controller = new LeadController(mockService);
+
+    String response = controller.home();
+    assertThat(response).contains("2 leads"); // MockLeadService возвращает 2 лида
   }
 
   @Test
   void shouldUseInjectedService() {
-    assertThat(controller).extracting("leadService").isSameAs(mockService);
+    MockLeadService mockService = new MockLeadService();
+    LeadController controller = new LeadController(mockService);
+
+    String response = controller.home();
+
+    assertThat(response).isNotNull();
+    assertThat(response).contains("Spring Boot CRM is running");
   }
 }
