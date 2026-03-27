b449bb7eacfe19391a6e78622b6cc1b595a6a2bc
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-03-27 14:32:26 +0300

    тесты проверки подключения

---
+++ src/test/java/ru/mentee/power/crm/repository/DatabaseConfigTest.java
@@ -0,0 +1,35 @@
+package ru.mentee.power.crm.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import ru.mentee.power.crm.spring.Application;
+
+@SpringBootTest(classes = Application.class)
+@ActiveProfiles("test")
+public class DatabaseConfigTest {
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @Test
+  void shouldConnectToH2Database() throws InterruptedException {
+    String databaseProductName = jdbcTemplate.execute((ConnectionCallback<String>) connection ->
+        connection.getMetaData().getDatabaseProductName()
+    );
+    assertThat(databaseProductName).isEqualTo("H2");
+  }
+
+  @Test
+  void shouldHaveLeadsTableCreated() {
+    Integer count = jdbcTemplate.queryForObject(
+        "Select COUNT(*) FROM information_schema.tables" +
+            " WHERE table_name='LEADS'", Integer.class);
+    assertThat(count).isEqualTo(1);
+  }
+}
