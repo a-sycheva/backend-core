784c7d401312b12735db3395f397cad4d2191b91
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-03-31 17:53:18 +0300

    добавила convertLeadToDeal в LeadService

--- src/main/java/ru/mentee/power/crm/service/LeadService.java
+++ src/main/java/ru/mentee/power/crm/service/LeadService.java
@@ -1,5 +1,6 @@
 package ru.mentee.power.crm.service;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -16,17 +17,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+import ru.mentee.power.crm.model.Deal;
 import ru.mentee.power.crm.model.Lead;
 import ru.mentee.power.crm.model.LeadStatus;
+import ru.mentee.power.crm.repository.DealRepository;
 import ru.mentee.power.crm.repository.LeadRepository;
 
 @Service
 public class LeadService {
   private static final Logger LOG = LoggerFactory.getLogger(LeadService.class);
-  private final LeadRepository repository;
+  private final LeadRepository leadRepository;
+  private final DealRepository dealRepository;
 
-  public LeadService(LeadRepository repository) {
-    this.repository = repository;
+  public LeadService(LeadRepository leadRepository, DealRepository dealRepository) {
+    this.leadRepository = leadRepository;
+    this.dealRepository = dealRepository;
+
     LOG.info("LeadService constructor called");
   }
 
@@ -37,7 +43,7 @@ public class LeadService {
 
   public Lead addLead(String name, String email, String company, LeadStatus status) {
 
-    Optional<Lead> existing = repository.findByEmail(email);
+    Optional<Lead> existing = leadRepository.findByEmail(email);
     if (existing.isPresent()) {
       throw new IllegalStateException("Lead with email already exists: " + email);
     }
@@ -49,12 +55,12 @@ public class LeadService {
         status
     );
 
-    return repository.save(lead);
+    return leadRepository.save(lead);
   }
 
   public Lead addLead(String email, String company, LeadStatus status) {
 
-    Optional<Lead> existing = repository.findByEmail(email);
+    Optional<Lead> existing = leadRepository.findByEmail(email);
     if (existing.isPresent()) {
       throw new IllegalStateException("Lead with email already exists: " + email);
     }
@@ -65,12 +71,12 @@ public class LeadService {
         status
     );
 
-    return repository.save(lead);
+    return leadRepository.save(lead);
   }
 
   public Lead update(UUID id, Lead updatedLead) {
 
-    Optional<Lead> existing = repository.findById(id);
+    Optional<Lead> existing = leadRepository.findById(id);
     if (existing.isEmpty()) {
       throw new ResponseStatusException(
           HttpStatus.NOT_FOUND,
@@ -82,42 +88,42 @@ public class LeadService {
     existing.get().setCompany(updatedLead.company());
     existing.get().setStatus(updatedLead.status());
 
-    return repository.save(existing.get());
+    return leadRepository.save(existing.get());
   }
 
   public void delete(UUID id) {
-    if (repository.findById(id).isEmpty()) {
+    if (leadRepository.findById(id).isEmpty()) {
       throw new ResponseStatusException(HttpStatus.NOT_FOUND,
           "Lead with id = " + id + "not exists!");
     } else {
-      repository.deleteById(id);
+      leadRepository.deleteById(id);
     }
   }
 
   public List<Lead> findAll() {
-    return repository.findAll();
+    return leadRepository.findAll();
   }
 
   public Optional<Lead> findById(UUID id) {
-    return repository.findById(id);
+    return leadRepository.findById(id);
   }
 
   public Optional<Lead> findByEmail(String email) {
-    return  repository.findByEmail(email);
+    return  leadRepository.findByEmail(email);
   }
 
   public List<Lead> findByStatuses(LeadStatus... statuses) {
-    return  repository.findByStatusIn(List.of(statuses));
+    return  leadRepository.findByStatusIn(List.of(statuses));
   }
 
   public List<Lead> findByStatus(LeadStatus status) {
-    return  repository.findAll().stream()
+    return  leadRepository.findAll().stream()
         .filter(lead -> lead.status().equals(status))
         .collect(Collectors.toList());
   }
 
   public List<Lead> findLeads(String name, String email, String company, LeadStatus status) {
-    Stream<Lead> stream = repository.findAll().stream();
+    Stream<Lead> stream = leadRepository.findAll().stream();
     if (name != null && !name.isBlank()) {
       stream = stream.filter(lead -> lead.name().toLowerCase().contains(name.toLowerCase()));
     }
@@ -136,18 +142,18 @@ public class LeadService {
   public Page<Lead> searchByCompany (String company, int pageNumber, int pageSize) {
     PageRequest pageRequest = PageRequest.of(
         pageNumber, pageSize);
-    return repository.findByCompany(company, pageRequest);
+    return leadRepository.findByCompany(company, pageRequest);
   }
 
   public Page<Lead> getFirstPage(int pageSize) {
     PageRequest pageRequest = PageRequest.of(
         0, pageSize, Sort.by("createdAt").descending());
-    return repository.findAll(pageRequest);
+    return leadRepository.findAll(pageRequest);
   }
 
   @Transactional
   public int convertNewToContacted() {
-    int updated = repository.updateStatusBulk(LeadStatus.NEW, LeadStatus.CONTACTED);
+    int updated = leadRepository.updateStatusBulk(LeadStatus.NEW, LeadStatus.CONTACTED);
     //логи
     System.out.printf("Converted %d leads from NEW to CONTACTED%n", updated);
     return updated;
@@ -155,6 +161,16 @@ public class LeadService {
 
   @Transactional
   public int archiveOldLeads(LeadStatus status) {
-    return repository.deleteByStatusBulk(status);
+    return leadRepository.deleteByStatusBulk(status);
+  }
+
+  @Transactional
+  public void convertLeadToDeal (UUID leadId, BigDecimal amount) {
+    if (leadRepository.existsById(leadId)) {
+      Deal deal = new Deal(leadId, amount);
+      dealRepository.save(deal);
+    } else {
+      throw new IllegalArgumentException("Lead not found: " + leadId);
+    }
   }
 }
