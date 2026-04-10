75034f9701c07106eba7fd34b8860fd8158f378a
Anastasiya Sycheva <a.a.sycheva95@yandex.ru>
2026-04-10 22:54:23 +0300

    сущность DealProduct, связь с Deal и Product

--- src/main/java/ru/mentee/power/crm/model/Product.java
+++ src/main/java/ru/mentee/power/crm/model/Product.java
@@ -1,13 +1,17 @@
 package ru.mentee.power.crm.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -39,6 +43,9 @@ public class Product {
 
   @Column(nullable = false)
   private Boolean active;
+
+  @OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
+  List<DealProduct> dealProducts = new ArrayList<>();
 
   public Product(String name, String sku, BigDecimal price, Boolean active) {
     this.name = name;
